a4610c3cd5708de980faca9cd5c1599af74eba4a
Mozzarto <37804591+Mozzarto@users.noreply.github.com>
2018-06-28 00:42:14 +0200

    Update Wyniki.java

--- src/gui/Wyniki.java
+++ src/gui/Wyniki.java
@@ -180,14 +180,14 @@ public class Wyniki {
 			if(name.equals("Kot")) {lblNewLabel_10 = new JLabel("");}
 			else {
 			if(((Pies) sorted.get(i)).getWielkosc()==1) {
-				lblNewLabel_10 = new JLabel("Ma≥y");
+				lblNewLabel_10 = new JLabel("Ma≈Çy");
 				
 			}
 			else if(((Pies) sorted.get(i)).getWielkosc()==2) {
-				lblNewLabel_10 = new JLabel("åredni");				
+				lblNewLabel_10 = new JLabel("≈öredni");				
 			}
 			else if(((Pies) sorted.get(i)).getWielkosc()==3) {
-				lblNewLabel_10 = new JLabel("Duøy");			
+				lblNewLabel_10 = new JLabel("Du≈ºy");			
 			}
 			}
 			lblNewLabel_10.setFont(new Font("Tahoma", Font.PLAIN, 11));
@@ -206,7 +206,7 @@ public class Wyniki {
 			int j=i+1;
 			if(User.getZaloguj()==1)
 			{
-				btnAdoptuj = new JButton("USU—");
+				btnAdoptuj = new JButton("USU≈É");
 				btnAdoptuj.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
 						frame.dispose();
@@ -370,5 +370,3 @@ public class Wyniki {
 		}
 		}
 	}
-
-
