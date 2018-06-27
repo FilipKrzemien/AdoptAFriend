1d72c2578079f20999cebed5e4bbef8e0e04620a
kurdesz <ka.kwilosz@gmail.com>
2018-06-28 00:58:58 +0200

    dodano testy jednostkowe/modyfikacja Testy

--- src/testy/Testy.java
+++ src/testy/Testy.java
@@ -5,27 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import junit.framework.Assert;
-
 class Testy {
 
 	@Test
 	public void testCreate() {
 		Kot kicia = new Kot("Mruczek",2,"Samiec","Czarny","Zielony","Dachowiec","krótka","02/04/2018","Blablabla");
-		Assert.assertNotNull(kicia);
-		Assert.assertEquals("Mruczek", kicia.getImie());
-		Assert.assertEquals(2, kicia.getWiek());
-		Assert.assertEquals("Samiec", kicia.getPlec());
-		Assert.assertEquals("Czarny", kicia.getKolor());
+		assertNotNull(kicia);
+		assertEquals("Mruczek", kicia.getImie());
+		assertEquals(2, kicia.getWiek());
+		assertEquals("Samiec", kicia.getPlec());
+		assertEquals("Czarny", kicia.getKolor());
 		kicia.setKolorOczu("Czarny");
-		Assert.assertEquals("Czarny", kicia.getKolorOczu());
+		assertEquals("Czarny", kicia.getKolorOczu());
 		kicia.setRasa("Bombajski");
-		Assert.assertEquals("Bombajski", kicia.getRasa());
-		
-	}
-
-	@Test
-	public void test2() {
+		assertEquals("Bombajski", kicia.getRasa());
 		
 	}
 }
